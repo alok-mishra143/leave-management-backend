dc129ee347aa948117720e2b15d84d27d2b2ad53
Alok Mishra <21se02cs042@ppsu.ac.in>
2025-03-21 09:55:44 +0530

    added env

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 9000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.NEXTJS_URL!,
     credentials: true,
   })
 );
